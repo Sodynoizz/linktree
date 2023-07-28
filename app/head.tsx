ac0a1617d122031f9c1b66e12ef54689037aab5d
Sodynoizz_TH <tepza5556789@gmail.com>
2023-07-28 21:00:15 +0700

    fix: meta tag

--- app/head.tsx
+++ app/head.tsx
@@ -2,9 +2,12 @@ export default function Head() {
   return (
     <>
       <title>Linktree</title>
-      <meta name="description" content="Next.js + Tailwind CSS" />
+      <meta name="description" content="Sodynoizz Linktree" />
       <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="og:image" content="https://i.imgur.com/Lyxl04B.png" />
+      <meta name="og:title" content="Sodynoizz" />
+      <meta name="og:description" content="A sodynoizz linktree website"/>
     </>
   );
 }
