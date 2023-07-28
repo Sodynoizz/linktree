6450fd1b69a2a6f77c7dd80a37493e7f6cc1e702
Sodynoizz_TH <tepza5556789@gmail.com>
2023-07-28 21:06:40 +0700

    fix: theme-color

--- app/head.tsx
+++ app/head.tsx
@@ -5,8 +5,9 @@ export default function Head() {
       <meta name="description" content="Sodynoizz Linktree" />
       <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#cf1bbe"/>
       <meta name="og:image" content="https://i.imgur.com/Lyxl04B.png" />
-      <meta name="og:title" content="Sodynoizz" />
+      <meta name="og:title" content="Sodynoizz Linktree" />
       <meta name="og:description" content="A sodynoizz linktree website"/>
     </>
   );
