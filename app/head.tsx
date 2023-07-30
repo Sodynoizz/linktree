91bda8b7df3d4c9a100858edc6c1040e107e6614
Sodynoizz_TH <tepza5556789@gmail.com>
2023-07-30 23:38:52 +0700

    fix: icon again

--- app/head.tsx
+++ app/head.tsx
@@ -3,14 +3,13 @@ export default function Head() {
     <>
       <title>Sodynoizz</title>
       <meta name="description" content="Sodynoizz Linktree" />
-      <link rel="icon" type="image/x-icon" href="/assets/icon.jpg" />
-      <link rel="shortcut icon" type="image/x-icon" href="/assets/icon.jpg" />
+      <link rel="icon" type="image/jpg" href="/assets/icon.jpg" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#cf1bbe"/>
       <meta name="og:image" content="https://i.imgur.com/Lyxl04B.png" />
       <meta name="og:title" content="Sodynoizz Linktree" />
       <meta name="og:description" content="A sodynoizz linktree website"/>
-      <link rel="apple-touch-icon" href="/assets/icon.jpg"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="/assets/icon.jpg" />
       <link rel="apple-touch-icon" sizes="72x72" href="/assets/icon.jpg"/>
       <link rel="apple-touch-icon" sizes="114x114" href="/assets/icon.jpg"/>
     </>
