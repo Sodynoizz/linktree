1363ad4a89c33d26e7a9b0e45acd700e326fa45e
Sodynoizz_TH <tepza5556789@gmail.com>
2023-07-30 23:21:34 +0700

    fix: support icon for ios device

--- app/head.tsx
+++ app/head.tsx
@@ -9,6 +9,9 @@ export default function Head() {
       <meta name="og:image" content="https://i.imgur.com/Lyxl04B.png" />
       <meta name="og:title" content="Sodynoizz Linktree" />
       <meta name="og:description" content="A sodynoizz linktree website"/>
+      <link rel="apple-touch-icon" href="/assets/icon.jpg"/>
+      <link rel="apple-touch-icon" sizes="72x72" href="/assets/icon.jpg"/>
+      <link rel="apple-touch-icon" sizes="114x114" href="/assets/icon.jpg"/>
     </>
   );
 }
